3ad0dd2ebffd6006b0477c38f31543a1611b0249
BianchiPedro <pedro-bianchi@puc-rio.br>
2026-05-28 13:36:27 -0300

    feat text area

--- src/app/side-bar-right/side-bar-right.ts
+++ src/app/side-bar-right/side-bar-right.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
-import { MatTab, MatTabsModule } from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatIcon } from "@angular/material/icon";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-side-bar-right',
-  imports: [MatTabsModule, MatIcon],
+  imports: [MatTabsModule, MatIcon, FormsModule],
   templateUrl: './side-bar-right.html',
   styleUrl: './side-bar-right.css' 
 })
-export class SideBarRight {}
+export class SideBarRight {
+
+  message = '';
+  chatMessages: {text: String, author: String, date: Date}[] = [];
+
+  sendMessage() {
+    if (this.message.trim()) {
+      this.chatMessages.push({text: this.message, author: 'Master', date: new Date()})
+
+      this.message = '';
+    }
+  }
+
+}
