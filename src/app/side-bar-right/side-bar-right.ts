60b895528406a599439e9b09dada9ba362323e74
BianchiPedro <pedro-bianchi@puc-rio.br>
2026-05-28 15:10:59 -0300

    FEAT: DICE
    dice rolling and dice roll window on chat

--- src/app/side-bar-right/side-bar-right.ts
+++ src/app/side-bar-right/side-bar-right.ts
@@ -13,16 +13,31 @@ import { CommonModule } from '@angular/common';
 export class SideBarRight {
 
   message = '';
-  chatMessages: {text: String, author: String, date: Date}[] = [];
+  chatMessages: {
+    text?: string, 
+    author: string, 
+    date: Date, 
+    isRoll?: boolean, 
+    rollData?: { dice1: number, dice2: number, modifier: number, total: number, outcome: string }
+    }[] = [];
 
   sendMessage() {
+
+    //CLEAR COMMAND
     if (this.message.trim() === '/clear') {
       this.chatMessages = [];
       this.message = '';
       return
     }
+
+    // ROLL COMAND
+    if (this.message.trim().startsWith('/roll')) {
+      this.onRoll();
+    }
+
+    // COMMON TEXT
     if (this.message.trim()) {
-      this.chatMessages.push({text: this.message, author: '', date: new Date()})
+      this.chatMessages.push({text: this.message, author: 'Master (GM)', date: new Date()})
 
       this.message = '';
     }
@@ -35,5 +50,51 @@ export class SideBarRight {
       keyboardEvent.preventDefault();
       this.sendMessage();
     }
+  }
+
+  onRoll() {
+      const modificadorString = this.message.replace('/roll', '').replace(/\s+/g, '');
+      const modifier = parseInt(modificadorString) || 0;
+
+      const dice1 = Math.floor(Math.random() * 12) +1;
+      const dice2 = Math.floor(Math.random() * 12) +1;
+
+      const total = dice1 + dice2 + modifier;
+
+      let textModifier = '';
+      if (modifier > 0) {
+        textModifier = ` + ${modifier}`;
+      } else if (modifier < 0) {
+        textModifier = ` - ${Math.abs(modifier)}`;
+      }
+
+      let outcomeText = '';
+
+      if (total < 0) {
+        outcomeText = 'FALHA CRÍTICA...'
+      } else if (total <= 12) {
+        outcomeText = 'FALHA'
+      } else if (total <= 19) {
+        outcomeText = 'SUCESSO PARCIAL'
+      } else if (total <= 24) {
+        outcomeText = 'SUCESSO'
+      } else {
+        outcomeText = 'SUCESSO CRÍTICO!!'
+      }
+
+      this.chatMessages.push({ 
+        author: 'System', 
+        date: new Date(),
+        isRoll: true,
+        rollData: {
+          dice1: dice1,
+          dice2: dice2,
+          modifier: modifier,
+          total: total,
+          outcome: outcomeText
+        }
+      });
+
+      this.message = '';
   }
 }
