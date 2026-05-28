be390314d3e4271e111f07d4358cc1afb3ebe6b8
BianchiPedro <pedro-bianchi@puc-rio.br>
2026-05-28 14:07:01 -0300

    FEAT CHAT
    sending messages on enter, date, picture

--- src/app/side-bar-right/side-bar-right.ts
+++ src/app/side-bar-right/side-bar-right.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIcon } from "@angular/material/icon";
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-side-bar-right',
-  imports: [MatTabsModule, MatIcon, FormsModule],
+  imports: [MatTabsModule, MatIcon, FormsModule, CommonModule],
   templateUrl: './side-bar-right.html',
   styleUrl: './side-bar-right.css' 
 })
@@ -16,10 +17,18 @@ export class SideBarRight {
 
   sendMessage() {
     if (this.message.trim()) {
-      this.chatMessages.push({text: this.message, author: 'Master', date: new Date()})
+      this.chatMessages.push({text: this.message, author: '', date: new Date()})
 
       this.message = '';
     }
   }
 
+  onEnter(event: Event) {
+    const keyboardEvent = event as KeyboardEvent;
+
+    if (!keyboardEvent.shiftKey) {
+      keyboardEvent.preventDefault();
+      this.sendMessage();
+    }
+  }
 }
