c2eb040c7d62ecf96e97bc06e3bc94f14f361285
BianchiPedro <pedro-bianchi@puc-rio.br>
2026-06-02 10:21:54 -0300

    feat: Character Sheet basics Component

---
+++ src/app/side-bar-right/character-sheet-modal/character-basics/character-basics.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon'; 
+
+@Component({
+  selector: 'app-character-basics',
+  standalone: true,
+  imports: [CommonModule, FormsModule, MatIconModule], // Importante trazer esses módulos!
+  templateUrl: './character-basics.html'
+})
+export class CharacterBasics {
+  
+  // Array para guardar as 4 escolhas do jogador (iniciam vazias)
+  rotulosSelecionados = ['', '', '', ''];
+
+  // O "Banco de Dados" das opções disponíveis e seus ícones do Material Icons
+  opcoesRotulos = [
+    { nome: 'Novato', icone: 'eco' },
+    { nome: 'Aprendiz', icone: 'hardware' },
+    { nome: 'Adepto', icone: 'swords' },
+    { nome: 'Especialista', icone: 'eco' },
+    { nome: 'Mestre', icone: 'eco' },
+    { nome: 'Grão-mestre', icone: 'eco' }
+  ];
+
+  // Função que o HTML vai chamar para saber qual ícone desenhar
+  getIcone(nomeSelecionado: string): string {
+    const opcao = this.opcoesRotulos.find(opt => opt.nome === nomeSelecionado);
+    return opcao ? opcao.icone : 'eco';
+  }
+}
--- src/app/side-bar-right/character-sheet-modal/character-sheet-modal.ts
+++ src/app/side-bar-right/character-sheet-modal/character-sheet-modal.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CdkDrag } from "@angular/cdk/drag-drop";
 import { MatIcon } from "@angular/material/icon";
 import { MatIconButton } from '@angular/material/button';
+import { CharacterBasics } from './character-basics/character-basics';
 
 @Component({
   selector: 'app-character-sheet-modal',
-  imports: [CdkDrag, MatIcon, MatIconButton],
+  imports: [CdkDrag, MatIcon, CharacterBasics],
   templateUrl: './character-sheet-modal.html',
 })
 export class CharacterSheetModal {
