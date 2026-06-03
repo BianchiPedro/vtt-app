f5e3fa1ba26529d527b909566540b22062e84ad6
BianchiPedro <pedro-bianchi@puc-rio.br>
2026-06-03 11:31:42 -0300

    feat Character Relations Component

--- src/app/side-bar-right/character-sheet-modal/character-sheet-modal.ts
+++ src/app/side-bar-right/character-sheet-modal/character-sheet-modal.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CdkDrag, CdkDragHandle } from "@angular/cdk/drag-drop";
 import { MatIcon } from "@angular/material/icon";
 import { CharacterBasics } from './character-basics/character-basics';
+import { CharacterRelations } from "./character-relations/character-relations";
 
 @Component({
   selector: 'app-character-sheet-modal',
-  imports: [CdkDrag, MatIcon, CharacterBasics, CdkDragHandle],
+  imports: [CdkDrag, MatIcon, CharacterBasics, CdkDragHandle, CharacterRelations],
   templateUrl: './character-sheet-modal.html',
 })
 export class CharacterSheetModal {
